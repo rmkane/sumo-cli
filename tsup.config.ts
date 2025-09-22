03cafdf080ff99d8062308fda02d4d42bbab259f
rmkane <rmkane@proton.me>
2025-09-21 23:54:12 -0400

    Cleanup tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,26 +1,13 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig([
-  {
-    entry: ['src/index.ts'],
-    format: ['esm'],
-    target: 'node18',
-    outDir: 'dist',
-    clean: true,
-    splitting: false,
-    sourcemap: true,
-    minify: false,
-    dts: false,
-  },
-  {
-    entry: ['src/cli/index.ts'],
-    format: ['esm'],
-    target: 'node18',
-    outDir: 'dist/cli',
-    clean: false,
-    splitting: false,
-    sourcemap: true,
-    minify: false,
-    dts: false,
-  },
-])
+export default defineConfig({
+  entry: ['src/cli/index.ts'],
+  format: ['esm'],
+  target: 'node18',
+  outDir: 'dist',
+  clean: true,
+  splitting: false,
+  sourcemap: true,
+  minify: false,
+  dts: false,
+})
